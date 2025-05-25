11a7cd6d828ffb1fd302864dfc8723a4a0a3dc34
EMMMABK <adilbikiev2@gmail.com>
2025-05-25 19:57:32 +0600

    add CSS

--- advanced-authorization/src/index.js
+++ advanced-authorization/src/index.js
@@ -1,6 +1,12 @@
-import { createRoot } from 'react-dom/client';
-import App from './components/App';
-
-const container = document.getElementById('root');
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./components/App";
+import 'bootstrap/dist/css/bootstrap.min.css'
+const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
