5cc73138bb284204067587f92d7d11e137cea37c
EMMMABK <adilbikiev2@gmail.com>
2025-05-25 20:11:47 +0600

    add SignUp CSS

--- advanced-authorization/src/components/Signup.js
+++ advanced-authorization/src/components/Signup.js
@@ -23,7 +23,7 @@ export default function Signup() {
                             <Form.Label>Password Confirmation</Form.Label>
                             <Form.Control type='password' ref={passwordConfirmRef} required/>
                         </Form.Group>
-                        <Button className='w-100' type='submit'>Sign Up</Button>
+                        <Button className='w-100' type='submit' style={{ marginTop: "10px" }}>Sign Up</Button>
                     </Form>
                 </Card.Body>
             </Card> 
