afc465acfce28691edb385ec590f33081af25320
EMMMABK <adilbikiev2@gmail.com>
2025-05-27 00:10:00 +0600

    add Fixing Bugs

--- advanced-authorization/src/components/App.js
+++ advanced-authorization/src/components/App.js
@@ -2,33 +2,48 @@ import React from "react";
 import Signup from "./Signup";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
-import ForgotPassword from "./ForgotPassword"
+import ForgotPassword from "./ForgotPassword";
+import UpdateProfile from "./UpdateProfile";
 import { Container } from "react-bootstrap";
-import { AuthProvider } from "../contexts/AuthContext"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
   return (
     <AuthProvider>
-      <Container 
+      <Container
         className="d-flex align-items-center justify-content-center"
         style={{ minHeight: "100vh" }}
       >
-        <div className="w-100" style={{ maxWidth: "400px"}}>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
           <Router>
-            <AuthProvider>
-              <Routes>
-                <Route exact path="/" Component={Dashboard}/>
-                <Route path="/signup" Component={Signup}/>
-                <Route path="/login" Component={Login}/>
-                <Route path="/forgot-password" Component={ForgotPassword}/>
-              </Routes>
-            </AuthProvider>
+            <Routes>
+              <Route 
+                path="/" 
+                element={
+                  <PrivateRoute>
+                    <Dashboard />
+                  </PrivateRoute>
+                }
+              />
+              <Route 
+                path="/update-profile" 
+                element={
+                  <PrivateRoute>
+                    <UpdateProfile />
+                  </PrivateRoute>
+                }
+              />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+            </Routes>
           </Router>
         </div>
       </Container>
     </AuthProvider>
-  )
+  );
 }
 
 export default App;
